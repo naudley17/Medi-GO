d2cf3cf7abdbbc7d03e0e78781cd66e968eff498
Frostberg <andrest07@yahoo.com>
2021-06-21 22:47:46 +0700

    Update to Initialize.sql, was missing create database

--- Database/Initialize.sql
+++ Database/Initialize.sql
@@ -1,4 +1,5 @@
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
+CREATE DATABASE IF NOT EXISTS MediGo;
 CREATE TABLE IF NOT EXISTS `MediGO`.`Answers` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `fever` TINYINT NOT NULL,
